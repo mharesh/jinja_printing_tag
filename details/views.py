0f49718a4ded67f8a1284c3afefd0a4f70f5658b
mharesh <hareshchowdary7997gmail.com>
2023-07-27 12:09:58 +0530

    first commit

--- details/views.py
+++ details/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def about(request):
-    d = {'name':'vineeth'}
+    d = {'name':'vineeth','age':19}
 
     return render(request,'about.html',context=d)
